a1f6b42ac8880e31d346cbf62500b5dfc234ffe7
Sonam Jaiswal <sonam0211@gmail.com>
2017-12-18 01:10:14 +0530

    files modified

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ import subprocess
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
+r = redis.StrictRedis(decode_responses=True, host='localhost', port=6379, db=0)
 
 def redis_mass_insertion():
     ps = subprocess.Popen(('python', 'redis_commands.py'), stdout=subprocess.PIPE)
     output = subprocess.check_output(('redis-cli', '--pipe'), stdin=ps.stdout)
 
 def top_stocks():
-    r = redis.StrictRedis(decode_responses=True, host='localhost', port=6379, db=0)
+    
     r.flushall()
     redis_mass_insertion()
     top_stocks = r.zrevrange(name='open', start=0, end=9, withscores=True )
     data = []
     for stock in top_stocks:
-        data.append(r.hgetall(int(stock[0])))
+        data.append(r.hgetall(stock[0].strip()))
     return data
 
 
@@ -29,15 +30,20 @@ class Root:
         tmpl = env.get_template('index.html')
         return tmpl.render(data = data)
 
+    @cherrypy.expose
+    def search(self,name):
+        search_result = r.hgetall(name)
+        tmpl = env.get_template('search.html')
+        return tmpl.render(data=search_result)
+
 conf = {
+        'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
+        },
         '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
-        },
-        '/generator': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
         },
         '/static': {
             'tools.staticdir.on': True,
--- redis_commands.py
+++ redis_commands.py
@@ -6,5 +6,5 @@ with open(csv_file, 'r') as csvfile:
     csvReader = csv.reader(csvfile)
     next(csvReader)
     for row in csvReader:
-        print('HMSET {} code \"{}\" name \"{}\" open \"{}\" high \"{}\" low \"{}\" close \"{}\"\r'.format(int(row[0]), row[0], row[1], row[4], row[5], row[6], row[7]))
-        print('ZADD open {} {}\r'.format(float(row[4]), int(row[0])))
+        print('HMSET \"{}\" code \"{}\" name \"{}\" open \"{}\" high \"{}\" low \"{}\" close \"{}\"\r'.format(row[1].strip(), row[0], row[1], row[4], row[5], row[6], row[7]))
+        print('ZADD open {} \"{}\"\r'.format(float(row[4]), row[1].strip()))
